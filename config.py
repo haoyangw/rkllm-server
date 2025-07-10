dcf1be5aea48282093fde937d6d5100dcf411ff0
Wang Haoyang <haoyangw@gmail.com>
2025-07-11 00:35:51 +0800

    Update config.py for Gemma LLM
    Update config values to suit running Gemma LLMs.
    
    Also remove obsolete USE_GPU param that has been removed from
      rkllm runtime v1.2.1.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ This file centralizes all configurable parameters to make them easier to manage.
 """
 
 # Model Configuration
-MODEL_PATH = "../model/Qwen3-0.6B-w8a8-opt1-hybrid1-npu3.rkllm"
+MODEL_PATH = "../models/unsloth-gemma-3-1b-it-w8a8.rkllm"
 LIBRARY_PATH = "./src/librkllmrt.so"  # Path to the RKLLM runtime library
 
 # Server Configuration
@@ -14,10 +14,9 @@ API_BASE_PATH = "/v1"
 API_KEY = "anything"  # Default API key for authentication (can be any string)
 
 # Model Parameters
-MAX_CONTEXT_LENGTH = 30000
-MAX_NEW_TOKENS = -1  # -1 means no limit
+MAX_CONTEXT_LENGTH = 16384
+MAX_NEW_TOKENS = 8192
 N_KEEP = 32
-USE_GPU = True
 IS_ASYNC = False
 
 # System Prompt (if any)
@@ -25,4 +24,4 @@ SYSTEM_PROMPT = "You are a helpful assistant."
 
 # Debug Configuration
 DEBUG_MODE = False
-LOG_LEVEL = 2  # 0: Error, 1: Warning, 2: Info, 3: Debug
+LOG_LEVEL = 0  # 0: Error, 1: Warning, 2: Info, 3: Debug
